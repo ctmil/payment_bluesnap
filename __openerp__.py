abe02f690bf8208e422ba4a1a1fa6ee2fdbcddad
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2016-07-05 12:13:45 -0300

    [REF] Renaming from mercadopago to bluesnap

--- __openerp__.py
+++ __openerp__.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Bluensap Payment Acquirer',
+    'name': 'BluenSnap Payment Acquirer',
     'category': 'Hidden',
     'summary': 'Payment Acquirer: MercadoPago Implementation',
     'version': '9.0.0.0.0',
-    'description': """MercadoPago Payment Acquirer""",
+    'description': """BlueSnap Payment Acquirer""",
     'author': 'Moldeo Interactive - www.moldeo.coop',
     'depends': ['payment'],
     'data': [
-        'views/mercadopago.xml',
+        'views/bluesnap.xml',
         'views/payment_acquirer.xml',
         'views/res_config_view.xml',
-        'data/mercadopago.xml',
+        'data/bluesnap.xml',
     ],
     'test': [
     ],
